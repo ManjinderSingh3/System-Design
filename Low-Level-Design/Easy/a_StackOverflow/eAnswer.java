753ba4e4e66ead5ae2bbfec9b8be5bebf2bd6d5b
Manjinder <msinghw3@gmail.com>
2025-07-25 23:55:19 -0500

    Worked on analysing fields in Question and ANswer entities

--- Low-Level-Design/Easy/a_StackOverflow/eAnswer.java
+++ Low-Level-Design/Easy/a_StackOverflow/eAnswer.java
@@ -1,4 +1,15 @@
 package Easy.a_StackOverflow;
+import java.util.UUID;
 
 public class eAnswer {
+    private final String id;
+    private final eUser author;
+    private final eQuestion question;
+
+    public eAnswer(eUser author, eQuestion question){
+        this.id = UUID.randomUUID().toString();
+        this.author = author;
+        this.question = question;
+
+    }
 }
--- Low-Level-Design/Easy/a_StackOverflow/eQuestion.java
+++ Low-Level-Design/Easy/a_StackOverflow/eQuestion.java
@@ -1,4 +1,20 @@
 package Easy.a_StackOverflow;
+import java.util.UUID;
+import java.util.Date;
 
 public class eQuestion {
+    private final String id;
+    private final eUser author;
+    private final String questionTitle;
+    private final String questionDescription;
+    private final Date questionPostingDate;
+
+
+    public eQuestion(eUser author, String questionTitle, String questionDescription){
+        this.id = UUID.randomUUID().toString();
+        this.author = author;
+        this.questionTitle = questionTitle;
+        this.questionDescription = questionDescription;
+        this.questionPostingDate = new Date();
+    }
 }
