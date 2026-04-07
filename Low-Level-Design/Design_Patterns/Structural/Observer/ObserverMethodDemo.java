6516e44ba6968e23cb24880945db698af3ffd63d
Manjinder <msinghw3@gmail.com>
2026-04-06 23:39:58 -0500

    Observer Pattern demo class

---
+++ Low-Level-Design/Design_Patterns/Structural/Observer/ObserverMethodDemo.java
@@ -0,0 +1,7 @@
+package Design_Patterns.Structural.Observer;
+
+public class ObserverMethodDemo {
+    public static void main(String[] args) {
+        System.out.println("Observer Design pattern Main class");
+    }
+}
