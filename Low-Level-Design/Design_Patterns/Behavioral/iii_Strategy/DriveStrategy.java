880af4fd1b8be6c425ec10eb562ac2de752365bb
Manjinder <msinghw3@gmail.com>
2026-04-10 23:43:55 -0500

    Strategy pattern boilerplate code

---
+++ Low-Level-Design/Design_Patterns/Behavioral/iii_Strategy/DriveStrategy.java
@@ -0,0 +1,5 @@
+package Design_Patterns.Behavioral.iii_Strategy;
+
+public interface DriveStrategy {
+    public void drive();
+}
--- Low-Level-Design/Design_Patterns/Behavioral/iii_Strategy/StrategyDemo.java
+++ Low-Level-Design/Design_Patterns/Behavioral/iii_Strategy/StrategyDemo.java
@@ -1,4 +1,7 @@
 package Design_Patterns.Behavioral.iii_Strategy;
 
 public class StrategyDemo {
+    public static void main(String[] args){
+        System.out.println("STrategy pattern client class");
+    }
 }
