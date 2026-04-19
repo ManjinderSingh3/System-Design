a6b134dac15ca7b0e980fab7988d1244e358f063
Manjinder <msinghw3@gmail.com>
2026-04-18 23:55:17 -0500

    Decorator pattern starter code

---
+++ Low-Level-Design/Design_Patterns/Structural/Decorator/DecoratorDemo.java
@@ -0,0 +1,7 @@
+package Design_Patterns.Structural.Decorator;
+
+public class DecoratorDemo {
+    public static void main(String[] args){
+        System.out.println("Decorator pattern client");
+    }
+}
