5f7de3ba9e32bcbec3d45dce616e205d556aea66
Manjinder <msinghw3@gmail.com>
2026-04-23 23:15:59 -0500

    Chain Of Responsibility pattern demo

---
+++ Low-Level-Design/Design_Patterns/Behavioral/ChainOfResponsibility/ChainOfResponsibilityDemo.java
@@ -0,0 +1,7 @@
+package Design_Patterns.Behavioral.ChainOfResponsibility;
+
+public class ChainOfResponsibilityDemo {
+    public static void main(String[] args){
+        System.out.println("Chain of Responsibility Demo");
+    }
+}
